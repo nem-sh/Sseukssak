32be15d1be34d7d696258e60e593057f9347148f
김소영 <soyeong.kim.95@gmail.com>
2020-10-16 17:42:36 +0900

    [Study] create electron project and test google-drive-api, resolves S03P31B304-55
    Signed-off-by: 김소영 <soyeong.kim.95@gmail.com>

---
+++ study/김소영/ssg-ssag/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="homepage-banner">
+  
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+  },
+
+  data: () => ({
+      //
+  })
+});
+</script>
