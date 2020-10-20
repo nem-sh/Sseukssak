063cf22f47cdc07372749c8aa5ede71b115e0f81
박인남 <pin954562@gmail.com>
2020-10-20 12:05:25 +0900

    [Feat] initialize project structure, resolves S03P31B304-69

---
+++ sseukssak/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>\
+    <Exp1/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Exp1 from '@/components/Exp1.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Exp1,
+  }
+}
+</script>
