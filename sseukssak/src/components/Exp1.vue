774caf686114ce474b959de81f33cae0c68a57a0
박인남 <pin954562@gmail.com>
2020-10-20 13:53:56 +0900

    [Fix] fix load node_module error and electron typescript build error

--- sseukssak/src/components/Exp1.vue
+++ sseukssak/src/components/Exp1.vue
@@ -1,12 +1,17 @@
 <template>
-  <h1>
-      예시입니다!
-  </h1>
+  <v-btn @click="test">
+      test
+  </v-btn>
 </template>
 
 <script>
+import fs from 'fs'
 export default {
-
+    methods:{
+        test() {
+            console.log(fs)
+        }
+    }
 }
 </script>
 
