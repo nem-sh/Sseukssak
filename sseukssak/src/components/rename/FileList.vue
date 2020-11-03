4762f77438034dde44801db7c677d317237fbd9c
조수미 <tnal1632@naver.com>
2020-11-03 12:41:37 +0900

    [Feat] add sweetalert2, resolves S03P31B304-200

--- sseukssak/src/components/rename/FileList.vue
+++ sseukssak/src/components/rename/FileList.vue
@@ -23,10 +23,13 @@
           @change="changeSelect"
           v-model="choiceList"
           :key="item.name"
-          :label="item.name"
           :value="item"
           color="#7288da"
-        ></v-checkbox>
+        >
+        <template v-slot:label>
+          <img height="20px" :src="fileIcon(item.type)"/>{{item.name}}
+        </template>
+      </v-checkbox>
       </template>
     </v-virtual-scroll>
   </v-col>
@@ -65,6 +68,71 @@ export default class Rename extends Vue {
   changeBeforeItems!: (item: FileInfo[]) => void;
   sortBeforeItems!: () => void;
   initailizeRename!: () => void;
+  fileType: object = {
+    "Document": [
+      "ppt",
+      "pptx",
+      "doc",
+      "docx",
+      "xls",
+      "pdf",
+      "ai",
+      "pad",
+      "hwp",
+      "txt",
+      "md",
+    ],
+    "Image": [
+      "jpg",
+      "jpeg",
+      "jpe",
+      "gif",
+      "png",
+      "bmp",
+      "rle",
+      "dib",
+      "psd",
+      "pdd",
+      "raw",
+      "dcm",
+      "dc3",
+      "dic",
+      "eps",
+      "psb",
+      "pct",
+      "pict",
+      "pxr",
+      "pbm",
+      "pgm",
+      "pnm",
+      "pfm",
+      "pam",
+      "tiff",
+      "tif",
+      "cr2",
+      "srw",
+      "nrw",
+    ],
+    "Video": [
+      "avi",
+      "mpg",
+      "mpeg",
+      "mpe",
+      "wmv",
+      "asf",
+      "asx",
+      "flv",
+      "rm",
+      "mov",
+      "dat",
+      "mkv",
+      "flv",
+      "mov",
+      "mp4",
+    ],
+    "Audio": ["wav", "wma", "mp3"],
+    "Compressed": ["zip", "apk", "rar", "7z", "tar"],
+  };
 
   get partTitleMode() {
     return this.$vuetify.theme.dark? "part-title-d" : "part-title"
@@ -74,6 +142,21 @@ export default class Rename extends Vue {
   }
   get folderMode() {
     return this.$vuetify.theme.dark? "folder-d" : "folder"
+  }
+  fileIcon(type) {
+    if (this.fileType["Document"].includes(type)) {
+      return require('../../assets/docx_file.png')
+    } else if (this.fileType["Image"].includes(type)) {
+      return require('../../assets/image_file.png')
+    } else if (this.fileType["Video"].includes(type)) {
+      return require('../../assets/video_file.png')
+    } else if (this.fileType["Audio"].includes(type)) {
+      return require('../../assets/audio_file.png')
+    } else if (this.fileType["Compressed"].includes(type)) {
+      return require('../../assets/zip_file.png')
+    } else {
+      return require('../../assets/folder-icon.png')
+    }
   }
 
   async read() {
