0ee9ac6375dd84bb9c5d322ddf08382503a2a325
김소영 <soyeong.kim.95@gmail.com>
2020-11-04 16:45:13 +0900

    [Fix] hide menu to build
    Signed-off-by: 김소영 <soyeong.kim.95@gmail.com>

--- sseukssak/src/router/index.ts
+++ sseukssak/src/router/index.ts
@@ -1,39 +1,39 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '@/views/Home.vue';
-import Rename from '@/views/Rename.vue';
-import Restore from '@/views/Restore.vue';
-import MiniMode from '@/views/MiniMode.vue';
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "@/views/Home.vue";
+import Rename from "@/views/Rename.vue";
+import Restore from "@/views/Restore.vue";
+import MiniMode from "@/views/MiniMode.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/rename',
-    name: 'Rename',
-    component: Rename
+    path: "/rename",
+    name: "Rename",
+    component: Rename,
   },
   {
-    path: '/restore',
-    name: 'Restore',
-    component: Restore
+    path: "/restore",
+    name: "Restore",
+    component: Restore,
   },
   {
-    path: '/mini',
-    name: 'MiniMode',
-    component: MiniMode
-  }
+    path: "/mini",
+    name: "MiniMode",
+    component: MiniMode,
+  },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
