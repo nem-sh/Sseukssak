f1bdeb2dc9a56a794e8324661835f275e9df62d0
박명수 <huazun123@naver.com>
2020-11-14 19:23:23 +0900

    check mattermost

--- sseukssak/src/views/Restore.vue
+++ sseukssak/src/views/Restore.vue
@@ -8,9 +8,60 @@
         class="mt-4"
       />
     </div>
-    <hr />
+    <div style="helpbtn" class="mt-3" align="right">
+      <v-dialog width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon color="indigo lighten-1" dark v-bind="attrs" v-on="on">
+            <v-icon>far fa-question-circle</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <div class="text-center">
+              <span class="headline text-center">History 기능</span>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <span>
+              1. 좌측의 박스 색상을 통해 어떤 작업인지 바로 알 수 있습니다.
+              <hr />
+            </span>
+            <br />
+            <v-icon color="#78909c">fas fa-square</v-icon> 파일 이동 <br />
+            <v-icon color="#69f0ae">fas fa-square</v-icon> 중복 파일 체크 <br />
+            <v-icon color="#40c4ff">fas fa-square</v-icon> 파일 이름 변경 <br />
+            <v-icon color="#7c4dff">fas fa-square</v-icon> 구글 드라이브 이동
+            <br />
+            <br />
+
+            <div align="right">
+              <span>
+                2. 우측의 아이콘을 통해 해당 작업의 결과를 알 수 있습니다.
+                <hr /> </span
+              ><br />
+              <span>해당 작업에 성공했습니다. </span>
+              <v-icon color="teal lighten-1" size="10">fas fa-circle</v-icon>
+              <br />
+              <span>해당 작업에 실패했습니다. </span>
+              <v-icon color="red darken-1" size="10"
+                >fas fa-exclamation-circle</v-icon
+              >
+              <br />
+              <span>복구 작업을 진행하였습니다. </span>
+              <v-icon color="blue lighten-3" size="10">fas fa-undo-alt</v-icon>
+              <br />
+              <br />
+              <br />
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </div>
+    <br />
+
     <div
-      style="overflow-x:hidden; overflow-y:scroll; width:100%; height:400px;"
+      style="overflow-x:hidden; overflow-y:scroll;  height:410px;"
+      :class="scrollerBgMode"
     >
       <v-list v-for="(timechunk, time) in timesortedList" :key="time">
         <v-expansion-panels color="grey lighten-4" style="chunk">
@@ -22,41 +73,48 @@
           <v-expansion-panel
             v-for="chunk in timechunk"
             :key="chunk + Math.random()"
+            :class="{
+              worktype1: chunk.workcode == 1,
+              worktype2: chunk.workcode == 2,
+              worktype3: chunk.workcode == 3,
+              worktype4: chunk.workcode == 4
+            }"
           >
-            <div
-              :class="{
-                worktype1: chunk.workcode == 1,
-                worktype2: chunk.workcode == 2,
-                worktype3: chunk.workcode == 3,
-                worktype4: chunk.workcode == 4
-              }"
-            >
-              <!-- 성공 실패에 따른 카드 색상 변경 -->
+            <!-- 성공 실패에 따른 카드 색상 변경 -->
 
-              <v-expansion-panel-header
-                :class="{
-                  ss: chunk.success == 1,
-                  ff: chunk.success == 0,
-                  rr: chunk.success == -1
-                }"
+            <v-expansion-panel-header expand-icon=" ">
+              <span
+                class="d-inline-block text-truncate"
+                style="max-width: 300px;"
+                >파일명 : {{ chunk.filename }}</span
               >
-                <span
-                  class="d-inline-block text-truncate"
-                  style="max-width: 300px;"
-                  >파일명 : {{ chunk.filename }}</span
+              <div align="right">
+                <v-icon
+                  color="teal lighten-1"
+                  size="10"
+                  v-if="chunk.success == 1"
+                  >fas fa-circle</v-icon
                 >
-                <span style="text-align: right;"
-                  >작업분류 : {{ convertWorkcode(chunk.workcode) }}</span
+                <v-icon color="red darken-1" size="10" v-if="chunk.success == 0"
+                  >fas fa-exclamation-circle</v-icon
                 >
-              </v-expansion-panel-header>
-            </div>
-            <v-expansion-panel-content>
-              <span>
-                {{ chunk.success == 1 ? "해당 작업 성공" : "" }}
-                {{ chunk.success == 0 ? "해당 작업 실패" : "" }}
-                {{ chunk.success == -1 ? "복구 작업 성공" : "" }}
-              </span>
-              <br />
+                <v-icon
+                  color="blue lighten-3"
+                  size="10"
+                  v-if="chunk.success == -1"
+                  >fas fa-undo-alt</v-icon
+                >
+              </div>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content style="chunkcontent">
+              <hr />
+              <span style="text-align: right;" class="d-inline-block"
+                >요약 : {{ convertWorkcode(chunk.workcode) }}
+                {{ chunk.success == -1 ? "복구 작업" : ""
+                }}{{ chunk.success == 1 ? "성공" : ""
+                }}{{ chunk.success == 0 ? "실패" : "" }}</span
+              >
+              <hr />
               <!-- 텍스트 길이가 길어지면 ...으로 표현하도록 하였음 -->
               <!-- 해당 속성은 text-truncate이며 필요없다면 삭제 -->
               <span
@@ -64,7 +122,6 @@
                 style="max-width: 700px;"
                 >이동 전 위치 : {{ chunk.before }}</span
               >
-              <br />
               <span
                 class="d-inline-block text-truncate"
                 style="max-width: 700px;"
@@ -82,44 +139,52 @@
 </template>
 
 <style>
+.helpbtn {
+  float: right;
+}
+
+.helpmodal {
+  text-align: center;
+}
+
 .restorebtn {
   float: right;
   width: 500px;
 }
 /* 성공:ss, 실패:ff, 복구:rr로 할 예정 */
 .ss {
-  background-color: #6699ff;
+  color: greenyellow;
 }
 
 .ff {
-  background-color: #ff6699;
+  color: #ff6699;
 }
 .rr {
-  background-color: #99ff66;
+  color: teal;
 }
 
 .worktype1 {
   border: solid 0px;
   border-left-width: 15px;
-  border-left-color: black;
+  border-left-color: #78909c;
 }
 .worktype2 {
   border: solid 0px;
 
   border-left-width: 15px;
-  border-left-color: teal;
+  border-left-color: #69f0ae;
 }
 .worktype3 {
   border: solid 0px;
 
   border-left-width: 15px;
-  border-left-color: lightblue;
+  border-left-color: #40c4ff;
 }
 .worktype4 {
   border: solid 0px;
 
   border-left-width: 15px;
-  border-left-color: darkblue;
+  border-left-color: #7c4dff;
 }
 
 .smallchunk {
@@ -232,7 +297,7 @@ export default class Restore extends Vue {
 
     let d = new Date(Number(time)).getMinutes();
 
-    return `${a}월 ${b}일 ${c}시 ${d}분`;
+    return `${a + 1}월 ${b}일 ${c}시 ${d}분`;
   }
 
   selectchunk(t) {
