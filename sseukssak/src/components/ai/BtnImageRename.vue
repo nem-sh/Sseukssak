6a89461356f62d0b4f8c4720261ce7512924cf50
박인남 <pin954562@gmail.com>
2020-11-15 12:44:12 +0900

    build test

--- sseukssak/src/components/ai/BtnImageRename.vue
+++ sseukssak/src/components/ai/BtnImageRename.vue
@@ -1,4 +1,11 @@
 <template>
+  <!-- <div>
+    <form action="">
+
+    </form>
+  </div> -->
+  <div>
+
   <a
     @click="apiRequest"
     style="display: flex; align-items: center"
@@ -12,6 +19,16 @@
         alt="AI"
     />이미지 이름 자동 변경
   </a>
+  <!-- <form
+  target="_blank"
+  method="POST"
+  enctype="application/x-www-form-urlencoded"
+  action="https://postman-echo.com/post"
+>
+  <input type="text" name="--theKey">
+  <input type="submit">
+</form> -->
+  </div>
 </template>
 
 <script lang='ts'>
@@ -21,7 +38,9 @@ import fs from 'fs'
 import { mapGetters, mapMutations, mapState } from 'vuex'
 import Swal from 'sweetalert2'
 import Axios from 'axios'
-
+import FormData from 'form-data'
+// import {request} from 'http'
+import fetch from 'electron-fetch'
 
 const { shell } = require("electron").remote;
 
@@ -32,19 +51,36 @@ export default class BtnImageRename extends Vue {
     
     apiRequest() {
         const URL = 'https://dapi.kakao.com/v2/vision/multitag/generate'
-        const formData = new FormData();
-        formData.append('image',fs.readFileSync('test.jpg').toString())
-        const data = {
-            'image':fs.readFileSync('test.jpg')
-        }
+        const readStream = fs.createReadStream('test.jpg')
+        const form = new FormData();
+        
+        form.append('image',readStream)
+
         const headers = {
             Authorization:'KakaoAK 86fa802ad9319ae7223fcff9d2020718',
             'Content-Type':'multipart/form-data'
         }
-        console.log(data)
-        Axios.post(URL, formData, {headers:headers})
-            .then(res => console.log(res)) 
-            .catch(err => console.log(err))
+
+        fetch(URL,{method: 'POST', body: form, headers: headers})
+          .then(res => res.json())
+          .then(json => console.log(json))
+
+
+        // const req = request(
+        //   {
+        //   host: 'https://dapi.kakao.com',
+        //   path: '/v2/vision/multitag/generate',
+        //   method: 'POST',
+        //   headers: headers
+        //   },
+        //   res => {
+        //     console.log(res.statusCode)
+        //   }
+        // )
+        // form.pipe(req)
+        // Axios.post(URL, form, {headers:headers})
+        //     .then(res => console.log(res)) 
+        //     .catch(err => console.log(err))
     }
 
   
